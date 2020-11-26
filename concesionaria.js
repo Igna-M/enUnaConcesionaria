4f107f0f32a87dcc18a6c6cae8043730235b6c36
Igna-M <ignaciomanzo@Air-de-Ignacio.fibertel.com.ar>
2020-11-26 14:57:16 -0300

    Agregue la funcion autosParaLaVenta

--- concesionaria.js
+++ concesionaria.js
@@ -10,6 +10,12 @@ const concesionaria = {
         if (autoVendido != null){
             autoVendido.vendido = true
         }
+    },
+    autosParaLaVenta : function() {
+        let autoOk = this.autos.filter(function(elemento){
+            return elemento.vendido == false;
+        });
+        return autoOk
     }
 
 };
@@ -18,5 +24,6 @@ const concesionaria = {
 //console.log(concesionaria.autos);
 //console.log(concesionaria.buscarAuto('JJK116'));
 concesionaria.venderAuto('JJK116')
-concesionaria.venderAuto('APL123')
-console.log(concesionaria.autos);
+//concesionaria.venderAuto('APL123')
+//console.log(concesionaria.autos);
+console.log(concesionaria.autosParaLaVenta());
